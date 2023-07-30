c7e895ea8e4d4b78a76265aa500931d297400c09
Mukul pathak <mukulpathak4@gmail.com>
2023-07-30 13:42:59 +0530

    written first program

--- firstApp.js
+++ firstApp.js
@@ -1,6 +1,6 @@
 console.log("Hello this is first in node js");
 
 
-const fs =  require('fs');
+const fs = require('fs');
 
 fs.writeFileSync('Hello.txt' , 'Console.log("This file is created through another js file")');
